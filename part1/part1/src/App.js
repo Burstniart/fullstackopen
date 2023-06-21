a5e4d651dac605f38dc9a9b69b651b9779d37d53
Burstniart <v4gr33n@hotmail.com>
2023-06-20 23:11:03 -0600

    did some work

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,21 +1,29 @@
 
-function App() {
+function App(props) {
     const name = 'Lex'
     const age = 1
-  return (
+    const {counter} = props
+    return (
+/*	
     <div>
 	  <h1>Greetings</h1>
 	  <Hello name='Monika' age={age}/>
-	  <Hello name={name} age={12+11}/>
+	  <Hello name={name} age={14+11}/>
     </div>
+*/
+    <div>{counter}</div>
   );
 }
 
 const Hello = (props) => {
     console.log(props)
+    const age = props.age
+    const name = props.name
+    const bornYear = () => new Date().getFullYear() - age
     return (
      <div>
-            <p>Hello {props.name}! you are {props.age} years old</p>
+            <p>Hello {name}! you are {age} years old</p>
+	    <p>You're probably born in {bornYear()}</p>
      </div>
   )
 }
