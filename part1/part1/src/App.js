8cf3d1301bbf37ae0ffd28c0c95f1994414c90a5
Burstniart <v4gr33n@hotmail.com>
2023-06-21 21:51:52 -0600

    event handling

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,30 +1,18 @@
+import { useState } from 'react'
 
-function App(props) {
-    const name = 'Lex'
-    const age = 1
-    const {counter} = props
+function App() {
+    const [ counter, setCounter ] = useState(0)
+    
+    const handleClick = () => {
+	setCounter(counter + 1)
+	console.log('clicked', counter)
+    }
+    
     return (
-/*	
-    <div>
-	  <h1>Greetings</h1>
-	  <Hello name='Monika' age={age}/>
-	  <Hello name={name} age={14+11}/>
-    </div>
-*/
-    <div>{counter}</div>
-  );
-}
-
-const Hello = (props) => {
-    console.log(props)
-    const age = props.age
-    const name = props.name
-    const bornYear = () => new Date().getFullYear() - age
-    return (
-     <div>
-            <p>Hello {name}! you are {age} years old</p>
-	    <p>You're probably born in {bornYear()}</p>
-     </div>
+	<div>
+	    <div>{counter}</div>
+	    <button onClick={handleClick}>Plus</button>
+	</div>
   )
 }
 
